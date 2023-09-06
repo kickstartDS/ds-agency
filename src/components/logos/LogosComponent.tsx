b514d9d7eb3cfefd380bc9404232c10368faf40e
fleven <franz.leven@kickstartds.com>
2023-09-06 13:34:18 +0200

    feat(logoComponent): develop logo component

--- src/components/logos/LogosComponent.tsx
+++ src/components/logos/LogosComponent.tsx
@@ -14,33 +14,35 @@ export const Logos: FC<LogosProps & HTMLAttributes<HTMLElement>> = ({
   logosPerRow = "6",
 }) => {
   return (
-    <div className={classnames(`c-logos c-logos--align-${align}`)}>
-      <div className="c-logos__tagline">{tagline}</div>
-      <LogoTiles
-        className={classnames(`c-logo-tiles--cols-${logosPerRow}`)}
-        logos={logos}
-      />
+    <>
+      <div className={classnames(`c-logos c-logos--align-${align}`)}>
+        <div className="c-logos__tagline">{tagline}</div>
+        <LogoTiles
+          className={classnames(`c-logo-tiles--cols-${logosPerRow}`)}
+          logos={logos}
+        />
 
-      <div className="c-logos__cta">
-        <div className="c-logos__cta__text">
-          {cta?.text}
-          {cta?.style === "text" ? (
-            <>
-              &#32;
-              <Link className="c-logos__cta__link" href={cta.link}>
-                {cta.label}
-              </Link>
-            </>
+        <div className="c-logos__cta">
+          <div className="c-logos__cta__text">
+            {cta?.text}
+            {cta?.style === "text" ? (
+              <>
+                &#32;
+                <Link className="c-logos__cta__link" href={cta.link}>
+                  {cta.label}
+                </Link>
+              </>
+            ) : (
+              ""
+            )}
+          </div>
+          {cta?.style === "button" ? (
+            <Button href={cta.link} label={cta.label} />
           ) : (
             ""
           )}
         </div>
-        {cta?.style === "button" ? (
-          <Button href={cta.link} label={cta.label} />
-        ) : (
-          ""
-        )}
       </div>
-    </div>
+    </>
   );
 };
