fb90c34f0ca56cc2868f921cc90dba5f7faf740d
fleven <franz.leven@kickstartds.com>
2023-09-14 17:19:08 +0200

    feat(Stats): add icon funcionality to stats

--- src/components/stats/StatsComponent.tsx
+++ src/components/stats/StatsComponent.tsx
@@ -2,7 +2,7 @@ import { HTMLAttributes, FC } from "react";
 
 import { StatsProps } from "./StatsProps";
 import "./stats.scss";
-import { Picture } from "@kickstartds/base/lib/picture";
+import { Icon } from "@kickstartds/base/lib/icon";
 
 export const Stats: FC<StatsProps & HTMLAttributes<HTMLElement>> = ({
   stats = [],
@@ -11,7 +11,7 @@ export const Stats: FC<StatsProps & HTMLAttributes<HTMLElement>> = ({
     <div className="c-stats">
       {stats.map((stat, index) => (
         <div className="c-stats__item" key={index}>
-          {stat.img ? <Picture className="c-stats__icon" src={stat.img} /> : ""}
+          {stat.icon ? <Icon className="c-stats__icon" icon={stat.icon} /> : ""}
           <span className="c-stats__value">{stat.value}</span>
           <span className="c-stats__label">{stat.label}</span>
           {stat?.description ? (
