46ac78d9ffa58e776cc3ed86a96ec89c88697703
fleven <franz.leven@kickstartds.com>
2023-11-16 11:31:11 +0100

    feat(lughausen-theme): tune theme

--- src/playroom/themes/core/index.tsx
+++ src/playroom/themes/core/index.tsx
@@ -1,6 +1,6 @@
-import "../../../token/tokens.css";
+import "../../../token-dsa-new/tokens.css";
 import "../../../../static/index.css";
 
-import IconSprite from "../../../token/icons/IconSprite";
+import IconSprite from "../../../token-dsa-new/icons/IconSprite";
 
 export default () => <IconSprite />;
