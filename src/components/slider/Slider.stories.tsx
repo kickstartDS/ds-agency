5a3219cf4ffa01de47b634778a27448ff4dd155f
fleven <franz.leven@kickstartds.com>
2023-12-14 08:16:20 +0100

    feat(slider): update slider stories

--- src/components/slider/Slider.stories.tsx
+++ src/components/slider/Slider.stories.tsx
@@ -19,29 +19,53 @@ const meta: Meta = {
   },
   render: (args) => (
     <Slider {...args}>
-      <TeaserCard {...merge(TeaserCardStory.args, unpack(Card.args))} />
-      <TeaserCard {...merge(TeaserCardStory.args, unpack(Card.args))} />
-      <TeaserCard {...merge(TeaserCardStory.args, unpack(Card.args))} />
-      <TeaserCard {...merge(TeaserCardStory.args, unpack(Card.args))} />
-      <TeaserCard {...merge(TeaserCardStory.args, unpack(Card.args))} />
-      <TeaserCard {...merge(TeaserCardStory.args, unpack(Card.args))} />
+      <TeaserCard
+        image="img/mosaic-1.png"
+        headline="Innovative Solutions"
+        text="We provide innovative solutions to complex business problems. Our team is dedicated to helping your business grow."
+      />
+      <TeaserCard
+        image="img/mosaic-2.png"
+        headline="Expert Teams"
+        text="Our team of experts is always ready to provide top-notch services. We have the skills and experience to deliver exceptional results."
+      />
+      <TeaserCard
+        image="img/mosaic-3.png"
+        headline="Quality Services"
+        text="We are committed to providing quality services. We strive for excellence in everything we do."
+      />
+      <TeaserCard
+        image="img/mosaic-1.png"
+        headline="Customer Satisfaction"
+        text="Customer satisfaction is our top priority. We work hard to meet and exceed our clients' expectations."
+      />
+      <TeaserCard
+        image="img/mosaic-2.png"
+        headline="Continuous Improvement"
+        text="We believe in continuous improvement. We are always looking for ways to improve our services and deliver better results."
+      />
+      <TeaserCard
+        image="img/mosaic-3.png"
+        headline="Strategic Approach"
+        text="We take a strategic approach to solve complex problems. Our strategies are designed to deliver long-term success."
+      />
     </Slider>
   ),
 };
 export default meta;
 type Story = StoryObj<typeof Slider>;
 
-export const WithNavAndArrows: Story = {
-  args: pack({
-    arrows: true,
-    nav: true,
-  }),
-};
-
 export const MultiView: Story = {
   args: pack({
     perView: 3,
     gap: 20,
+  }),
+};
+
+export const WithNavAndArrows: Story = {
+  args: pack({
+    arrows: true,
+    nav: true,
   }),
 };
 
