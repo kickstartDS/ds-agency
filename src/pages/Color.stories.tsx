7674f44873dd774e1c06858379c32805966aea3b
fleven <franz.leven@kickstartds.com>
2024-02-15 10:03:37 +0100

    feat(tokenPreview): update template

--- src/pages/Color.stories.tsx
+++ src/pages/Color.stories.tsx
@@ -6,38 +6,153 @@ const Page = () => (
   <>
     <Section
       headline={{
-        text: "Background Color Primary",
+        text: "Color Primary",
+        large: true,
+      }}
+      backgroundColor="accent"
+      content={{
+        mode: "list",
       }}
     >
-      <div className="background-color background-color-primary">Static</div>
-      <Button
-        label="Interactive"
-        className="background-color background-color-primary-interactive"
-      />
+      <Section
+        headline={{
+          text: "Background Color Primary",
+        }}
+      >
+        <div className="color-primary background-color background-color">
+          Static
+        </div>
+        <Button
+          label="Interactive"
+          className="background-color color-interactive color-primary"
+        />
+      </Section>
+
+      <Section
+        headline={{
+          text: "Border Color Primary",
+        }}
+      >
+        <div className="border-color color-primary">Static</div>
+        <Button
+          label="Interactive"
+          className="border-color color-primary color-interactive"
+        />
+      </Section>
+
+      <Section
+        headline={{
+          text: "Text Color Primary",
+        }}
+      >
+        <div className="text-color color-primary">Static</div>
+        <Button
+          label="Interactive"
+          className="text-color color-primary color-interactive"
+        />
+      </Section>
     </Section>
 
     <Section
       headline={{
-        text: "Border Color Primary",
+        text: "Color Interface",
+        large: true,
+      }}
+      content={{
+        mode: "list",
       }}
     >
-      <div className="border-color border-color-primary">Static</div>
-      <Button
-        label="Interactive"
-        className="border-color border-color-primary-interactive"
-      />
+      <Section
+        headline={{
+          text: "Background Color Interface",
+        }}
+      >
+        <div className="color-interface background-color background-color">
+          Static
+        </div>
+        <Button
+          label="Interactive"
+          className="background-color color-interactive color-interface"
+        />
+      </Section>
+
+      <Section
+        headline={{
+          text: "Border Color Interface",
+        }}
+      >
+        <div className="border-color color-interface">Static</div>
+        <Button
+          label="Interactive"
+          className="border-color color-interface color-interactive"
+        />
+      </Section>
+
+      <Section
+        headline={{
+          text: "Text Color Interface",
+        }}
+      >
+        <div className="text-color color-interface">Static</div>
+        <Button
+          label="Interactive"
+          className="text-color color-interface color-interactive"
+        />
+      </Section>
     </Section>
 
     <Section
       headline={{
-        text: "Text Color Primary",
+        text: "Color Default",
+        large: true,
+      }}
+      backgroundColor="accent"
+      content={{
+        mode: "list",
       }}
     >
-      <div className="text-color text-color-primary">Static</div>
-      <Button
-        label="Interactive"
-        className="text-color text-color-primary-interactive"
-      />
+      <Section
+        headline={{
+          text: "Background Color Default",
+        }}
+        backgroundColor="accent"
+      >
+        <div className="color-default background-color background-color">
+          Static
+        </div>
+
+        <Button
+          label="Interactive"
+          className="background-color color-interactive color-default"
+        />
+      </Section>
+
+      <Section
+        headline={{
+          text: "Border Color Default",
+        }}
+        backgroundColor="accent"
+      >
+        <div className="border-color color-default">Static</div>
+
+        <Button
+          label="Interactive"
+          className="border-color color-default color-interactive"
+        />
+      </Section>
+
+      <Section
+        headline={{
+          text: "Text Color Default",
+        }}
+        backgroundColor="accent"
+      >
+        <div className="text-color color-default">Static</div>
+        <Button
+          label="Interactive"
+          className="text-color color-default color-interactive"
+        />
+      </Section>
     </Section>
   </>
 );
