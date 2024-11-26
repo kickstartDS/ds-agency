365664a661d0a1c0996fac9941307c476fa29f22
fleven <franz.leven@kickstartds.com>
2024-11-26 12:15:01 +0100

    feat(sd.config): fix icon sprite generation

--- sd.config.cjs
+++ sd.config.cjs
@@ -3,7 +3,10 @@ const StyleDictionary = require("style-dictionary");
 const { config } = require("@kickstartds/style-dictionary");
 
 module.exports = StyleDictionary.extend(config).extend({
-  source: ["src/token/dictionary/**/*.json"],
+  source: [
+    "src/token/dictionary/**/*.json",
+    path.join(__dirname, "token/dictionary/**/*.svg"),
+  ],
   platforms: {
     css: {
       buildPath: "src/token/",
